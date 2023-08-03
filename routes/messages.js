169cba30e4226e999a30079565faca4db4f535dc
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-08-03 16:49:48 -0400

    Created messageRoutes

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,9 @@
+const express = require("express");
+const ExpressError = require("../expressError");
+const Message = require("../models/message")
+
+const router = new express.Router()
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -10,6 +16,18 @@
  * Make sure that the currently-logged-in users is either the to or from user.
  *
  **/
+router.get("/:id", ensureLoggedIn, async function(req, res, next) {
+    try {
+        const id = req.params.id
+        const message = Message.get(id)
+        if(req.user.username === (await message).to_user.username || req.user.username === (await message).from_user_user.username){
+            return res.json({message: message})
+        }
+        throw new ExpressError("Unauthorized", 401)
+    } catch (error) {
+        return next(error)
+    }
+})
 
 
 /** POST / - post message.
@@ -18,6 +36,15 @@
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+router.post("/", ensureLoggedIn, async function(req, res, next) {
+    try {
+        const {to_username, body} = req.body
+        const message = await Message.create({from_username: req.user.username, to_username: to_username, body: body})
+        return res.json({message: message})
+    } catch (error) {
+        return next(error)
+    }
+})
 
 
 /** POST/:id/read - mark message as read:
@@ -27,4 +54,16 @@
  * Make sure that the only the intended recipient can mark as read.
  *
  **/
-
+router.post("/:id/read", ensureLoggedIn, async function(req, res, next) {
+    try {
+        const id = req.params.id
+        const checkMessage = await Message.get(id)
+        if(req.user.username === checkMessage.to_user.username){
+            const message = await Message.markRead(id)
+            return res.json({message: message})
+        }
+        throw new ExpressError("Unauthorized", 401)
+    } catch (error) {
+        return next(error)
+    }
+})
