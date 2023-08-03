1c1771e922545ac22cf47b26a019690056711ac1
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-08-03 16:52:51 -0400

    Applied middleware to userRoutes

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ const router = new express.Router()
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
-router.get("/", async function(req, res, next) {
+router.get("/", ensureLoggedIn, async function(req, res, next) {
     try {
         const users = await User.all()
         return res.json({users: users})
@@ -22,7 +22,7 @@ router.get("/", async function(req, res, next) {
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
-router.get("/:username", async function(req, res, next) {
+router.get("/:username", ensureCorrectUser, async function(req, res, next) {
     try {
         const username = req.params.username
         const user = await User.get(username)
@@ -41,7 +41,7 @@ router.get("/:username", async function(req, res, next) {
  *                 from_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
-router.get("/:username/to", async function(req, res, next) {
+router.get("/:username/to", ensureCorrectUser, async function(req, res, next) {
     try {
         const username = req.params.username
         const messages = await User.messagesTo(username)
@@ -60,7 +60,7 @@ router.get("/:username/to", async function(req, res, next) {
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
-router.get("/:username/from", async function(req, res, next) {
+router.get("/:username/from", ensureCorrectUser, async function(req, res, next) {
     try {
         const username = req.params.username
         const messages = await User.messagesFrom(username)
