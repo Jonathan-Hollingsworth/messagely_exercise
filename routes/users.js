4875bdf9f70e93ad94a1454e03095ff573e0803d
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-08-03 16:12:19 -0400

    Fixed error in routing

--- routes/users.js
+++ routes/users.js
@@ -60,7 +60,7 @@ router.get("/:username/to", async function(req, res, next) {
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
-router.get("/:username/to", async function(req, res, next) {
+router.get("/:username/from", async function(req, res, next) {
     try {
         const username = req.params.username
         const messages = await User.messagesFrom(username)
